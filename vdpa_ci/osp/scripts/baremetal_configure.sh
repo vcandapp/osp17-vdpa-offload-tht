705e83e24bbdfb901b6804338944d943af6a5d6b
vcandappa <vcandapp@redhat.com>
2023-06-12 13:40:09 +0530

    updates

--- vdpa_ci/osp/scripts/baremetal_configure.sh
+++ vdpa_ci/osp/scripts/baremetal_configure.sh
@@ -4,7 +4,7 @@ set -ex
 
 OPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-scp $OPT 17-dev-lab-scripts/osp/scripts_v2/baremetal_configure_remote.sh root@${server}:/root/
+scp $OPT vdpa_ci/osp/scripts/baremetal_configure_remote.sh root@${server}:/root/
 CMD="bash /root/baremetal_configure_remote.sh"
 ssh $OPT root@${server} "echo ${CMD}>>/root/auto-cmd-history"
 ssh $OPT root@${server} ${CMD}
