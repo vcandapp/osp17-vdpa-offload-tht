73e11b6a0c5efd65ac42df3c903396540e504158
vcandappa <vcandapp@redhat.com>
2023-06-12 13:43:39 +0530

    updates

--- vdpa_ci/osp/scripts/install_clean.sh
+++ vdpa_ci/osp/scripts/install_clean.sh
@@ -4,7 +4,7 @@ set -ex
 
 OPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-scp $OPT 17-dev-lab-scripts/osp/scripts_v2/install_clean_remote.sh root@${server}:/root/
+scp $OPT vdpa_ci/osp/scripts/install_clean_remote.sh root@${server}:/root/
 CMD="bash /root/install_clean_remote.sh ${server}"
 ssh $OPT root@${server} "echo ${CMD}>>/root/auto-cmd-history"
 ssh $OPT root@${server} ${CMD}
