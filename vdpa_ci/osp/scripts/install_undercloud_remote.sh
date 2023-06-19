d16c482a58db006111086f7f693ee7d6007513ac
vcandappa <vcandapp@redhat.com>
2023-06-19 17:34:26 +0530

    update uc domain

--- vdpa_ci/osp/scripts/install_undercloud_remote.sh
+++ vdpa_ci/osp/scripts/install_undercloud_remote.sh
@@ -24,7 +24,7 @@ REPO=""
 # Use undercloud SSL only with OSP16 onwards
 if [[ ${RELEASE} != "13" ]]; then
     # Facing error after installing shift-on-stack, fix it before enabling it
-    SSL="--ssl yes --tls-ca https://password.corp.redhat.com/RH-IT-Root-CA.crt"
+    SSL="--ssl no --tls-ca https://password.corp.redhat.com/RH-IT-Root-CA.crt"
     REPO="--repos-urls
 http://download.devel.redhat.com/rcm-guest/puddles/OpenStack/17.0-RHEL-9/latest-RHOS-17-RHEL-9.0/compose/OpenStack/x86_64/os/"
     #REPO="--repos-urls http://download-node-02.eng.bos.redhat.com/rhel-8/nightly/updates/FDP/latest-FDP-8-RHEL-8/compose/Server/x86_64/os/fdp-nightly-updates.repo"
@@ -42,23 +42,17 @@ inspection_iprange=$(awk -F "=" '/^inspection_iprange/{print $2;exit}' /root/inf
 #--repos-urls http://download.devel.redhat.com/rcm-guest/puddles/OpenStack/17.0-RHEL-8/latest-RHOS-17.0-RHEL-8.4/compose/OpenStack/x86_64/os/ \
 
 infrared tripleo-undercloud -vv \
-    -o undercloud.yml --mirror "brq" \
+    -o undercloud.yml --mirror "brq2" \
     --version $RELEASE --build ${BUILD} \
+    --splitstack no  --shade-host undercloud-0 \
     --boot-mode "uefi" \
     --images-task=rpm --images-update no ${SSL} ${REPO} \
-    --config-options DEFAULT.local_ip=${local_ip} \
-    --config-options DEFAULT.undercloud_public_host=${undercloud_public_host} \
-    --config-options DEFAULT.undercloud_admin_host=${undercloud_admin_host} \
-    --config-options ctlplane-subnet.cidr=${cidr} \
-    --config-options ctlplane-subnet.dhcp_start=${dhcp_start} \
-    --config-options ctlplane-subnet.dhcp_end=${dhcp_end} \
-    --config-options ctlplane-subnet.gateway=${gateway} \
-    --config-options ctlplane-subnet.inspection_iprange=${inspection_iprange} \
-    --config-options ctlplane-subnet.masquerade=true \
+    --config-file /root/infrared/undercloud.conf \
     --config-options DEFAULT.undercloud_timezone=UTC \
-    --tls-ca https://password.corp.redhat.com/RH-IT-Root-CA.crt
+    --ntp-pool clock.corp.redhat.com \
+    --tls-ca 'https://password.corp.redhat.com/RH-IT-Root-CA.crt'
 
-#--build RHOS-17.0-RHEL-8-20211105.n.0--tls-ca 'https://password.corp.redhat.com/RH-IT-Root-CA.crt'
+
 
 infrared ssh undercloud-0 "sudo yum install -y wget tmux vim"
 infrared ssh undercloud-0 "echo 'set-window-option -g xterm-keys on' >~/.tmux.conf"
